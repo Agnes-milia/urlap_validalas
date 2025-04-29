a9180146dbea5cf34969a24abec13d1f1a72bbd7
Agnes <68066824+Agnes-milia@users.noreply.github.com>
2025-04-29 09:39:56 +0200

    Update success.js

--- success.js
+++ success.js
@@ -3,15 +3,18 @@
 		const successMessage = document.querySelector('.success-message');
 
 		form.addEventListener('submit', function(event) {
-			event.preventDefault(); // prevent the form from submitting normally
+			event.preventDefault(); // default elküldés megakadályozása
 
-			// send the form data to the server using AJAX
+			// szervernek adatküldés AJAX-szal
 			const xhr = new XMLHttpRequest();
 			xhr.open('GET', 'action.php');
 			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 			xhr.onreadystatechange = function() {
+				// a readyState a dokumentum betöltési állapotát írja le
+				// A 4-es állapot azt jelenti, hogy a kérést elküldték, a szerver befejezte a válasz visszaküldését, 
+				// és a böngésző befejezte a választartalom letöltését, azaz az AJAX hívás befejeződött.
 				if (xhr.readyState === 4 && xhr.status === 200) {
-					// show the success message if the form submission was successful
+					// siker esetén megmutatja az üzenetet
 					successMessage.style.display = 'block';
 				}
 			};
